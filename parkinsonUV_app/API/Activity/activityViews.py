1dfa189aef46ed97c7bdd2226c438055af6dcea3
Joules <salamanca.julian@correounivalle.edu.co>
2023-08-18 00:15:18 -0500

    Actualizacion de ultimo momento

--- parkinsonUV_app/API/Activity/activityViews.py
+++ parkinsonUV_app/API/Activity/activityViews.py
@@ -69,45 +69,25 @@ class GetActivitiesByTherapistDetailed(APIView):
                 'id': activity.id,
                 'activity_name': activity.name, 
                 'description' : activity.description,
-                'interval': activity.interval,
-                'last_scheduled_date': activity.last_scheduled_date, 
-                'id_list' : activity.id_list_id, 
+                'start_date': activity.start_date, 
+                'end_date': activity.end_date,
+                'status': activity.status,
                 'id_patient' : activity.id_patient_id,
-                'id_therapist' : activity.id_therapist_id 
+                'id_type': activity.id_patient.id_type, 
+                'patient_name': activity.id_patient.name,
+                'patient_lastname': activity.id_patient.lastname,
+                'patient_email': activity.id_patient.email,
+                'patient_cell': activity.id_patient.cell,
+                'patient_age': activity.id_patient.age,
+                'patient_gender': activity.id_patient.gender, 
+                'patient_picture': activity.id_patient.user_id.user_picture,
+                'id_therapist' : activity.id_therapist_id, 
+                'therapist_name' : activity.id_therapist.name, 
+                'therapist_lastname' : activity.id_therapist.lastname,
+                'therapist_picture': activity.id_therapist.user_id.user_picture,
+                'id_list' : activity.id_list_id, 
+                'lista_name': activity.id_list.name
             }
-            print('Inspeccionemos', activity_data['id_list'])
-            ## Buscar el objeto correspondiente en result_patients 
-            patient = result_patients.filter(user_id = activity_data['id_patient'])
-            patient = list(patient.values())[0]
-            if patient: 
-                ## Convertimos la informacion del paciente a diccionario 
-                patient_data = {
-                    'id_patient' : patient['user_id_id'], 
-                    'id_type': patient['id_type'],
-                    'patient_name': patient['name'],
-                    'patient_lastname': patient['lastname'],
-                    'patient_email': patient['email'],
-                    'patient_cell': patient['cell'],
-                    'patient_age': patient['age'],
-                    'patient_gender': patient['gender'] 
-                }
-                
-                ## agregamos campos al diccionario principal: 
-                activity_data.update(patient_data)
-            
-            ## Buscar el objeto correspondiente en listas
-            lista = result_list.filter(id = activity_data['id_list'])
-            lista = list(lista.values())[0]
-
-            if lista: 
-                print(lista, 'VEAMOS QUE ES ESTO')
-                ## Convertir Lista a un diccionario 
-                lista_data = {
-                    'id_list': lista['id'],
-                    'lista_name': lista['name']
-                }
-                activity_data.update(lista_data)
-            
             result.append(activity_data)
         return Response(result) 
             
